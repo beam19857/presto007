88edcb90d6010095b061dd1d2450ef8a8e9682e0
cattdar1900 <towlovelove@gmail.com>
2019-07-28 18:28:43 +0700

    setting http request ver1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HomeServiceService } from './home-service.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
+  market;
+  marketlist = [];
 
-  constructor() {}
+  constructor(private HomeService : HomeServiceService ) {}
+
+
+  
+  ngOnInit(){
+      this.HomeService.getAccout().subscribe(
+        data => {
+          console.log("get Data");
+          console.log(data);
+          //this.market = JSON.stringify(data[0].id);
+          // data[value of id] . parameter of oject
+          for(var i = 0; i < data.length; i++){
+            this.marketlist.push(data[i]);
+            console.log((data[i])); //here you'll get sendernewcall value for all entry
+            console.log(this.marketlist);
+          }
+        }
+        
+      );
+
+      
+  }
+
 
 }
