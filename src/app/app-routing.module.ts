49ab1db0d768206c7d8edf04ec267520894f4fc9
cattdar1900 <towlovelove@gmail.com>
2019-07-31 19:38:08 +0700

    UI Update Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'model-page', loadChildren: './model-page/model-page.module#ModelPagePageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'model-page', loadChildren: './model-page/model-page.module#ModelPagePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
 
 ];
 
